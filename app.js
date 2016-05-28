6eacde571943e17acee4e5dfd5fdf850772b345f
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-05-28 21:02:16 +0200

    Enable saving and retrieving variables

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('Starting password manager');
+
+// Include node persist
+var storage = require('node-persist');
+storage.initSync();
+
+storage.setItemSync('name', 'Sandra'); //Sets the item and stores it
+
+var name = storage.getItemSync('name'); // This should retrieve the stored data
+console.log('Saved name is: ' + name); // This should show up in the terminal
+
