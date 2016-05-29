0465da12ff76ecd74b6bd7099657ee90a7cf8c31
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-05-29 11:10:09 +0200

    Setup account creation and storage

--- app.js
+++ app.js
@@ -4,21 +4,43 @@ console.log('Starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
-// storage.setItemSync('accounts', [{
 
-// 	username: "Sandra",
-// 	balance: 1000
+// Three attributes: account.name, account.username, account.password
+function createAccount (account) {
 
-// }]); //Sets the item and stores it
 
-var accounts = storage.getItemSync('accounts'); // This should retrieve the stored data
+	var accounts = storage.getItemSync('accounts')
 
-accounts.push({
-	username: 'Kelson',
-	balance: 1500
-});
+	if (typeof accounts ==='undefined') {
+		accounts = [];
+	}
 
-storage.setItemSync('accounts', accounts);
+	accounts.push(account)
+	storage.setItemSync('accounts', accounts);
 
-console.log(accounts); // This should show up in the terminal
+	return account;
+}
 
+function getAccount (accountName) {
+
+	var accounts = storage.getItemSync('accounts')
+	var matchedAccounts;
+
+	accounts.forEach(function (account) {
+		if (account.name === accountName) {
+			matchedAccount = account;
+		}
+	});
+
+	return matchedAccount;
+}
+
+// createAccount({
+// 	name: 'Sandra',
+// 	username: 'sandra',
+// 	password: 'password'
+
+// });
+
+var sandraAccount = getAccount('Sandra');
+console.log(sandraAccount);
