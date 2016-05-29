218cd428f3400345f372cc897789b832440c9be7
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-05-29 18:04:34 +0200

    Update require name and lastname

--- example-args.js
+++ example-args.js
@@ -1,28 +1,25 @@
 // Stores all arguments passed into the program
 var argv = require('yargs')
-	.command('hello', 'Greets the user', function(yargs) {
+	.command('hello', 'Greets the user', function (yargs) {
 
 		return yargs.options({
 			name: {
-				// Make name a required parameter
-				demand: true,
 				//To make our program easier to run, shorter to type.
 				alias: 'n',
 				description: 'Please enter your first name.'
 			}, 
 			lastname: {
-				// Make name a required parameter
-				demand: true,
-				//To make our program easier to run, shorter to type.
 				alias: 'l',
 				description: 'Please enter your last name.'
-			}
+			 }
 			// Help for this individual function
 		}).help('help');
 	})
 
 	// Global help function
 	.help('help')
+	// Make name and lastname required params
+	.demand(['n', 'l'])
 	.argv;
 // The _ stands for an array that takes arguments via the terminal
 var command = argv._[0];
