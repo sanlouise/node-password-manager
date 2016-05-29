1529170e03866d108fdcc5ffd9658dd4413661d6
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-05-29 17:43:48 +0200

    Enable last name entering

--- example-args.js
+++ example-args.js
@@ -5,11 +5,24 @@ var argv = require('yargs')
 		return yargs.options({
 			name: {
 				// Make name a required parameter
-				demand: true
+				demand: true,
+				//To make our program easier to run, shorter to type.
+				alias: 'n',
+				description: 'Please enter your first name.'
+			}, 
+			lastname: {
+				// Make name a required parameter
+				demand: true,
+				//To make our program easier to run, shorter to type.
+				alias: 'l',
+				description: 'Please enter your last name.'
 			}
-		});
+			// Help for this individual function
+		}).help('help');
 	})
 
+	// Global help function
+	.help('help')
 	.argv;
 // The _ stands for an array that takes arguments via the terminal
 var command = argv._[0];
