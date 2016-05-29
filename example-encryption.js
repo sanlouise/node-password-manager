d604af858c38f0fbf14011e694f6e9337767ed07
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-05-29 21:41:32 +0200

    Update example manner to encrypt username and password

--- example-encryption.js
+++ example-encryption.js
@@ -1,17 +1,18 @@
 var crypto = require('crypto-js');
+var secretMessage = {
+	name: 'Sandra',
+	secretName: 'San'
+}
+var secretKey = '123abc'
 
-var secretMessage = 'I hid the snacks.';
-var secretKey = '123abc';
 
-
-// Encrypt message
-// AES is an encryption method. Encrypt takes two arguments.
-var encryptedMessage = crypto.AES.encrypt(secretMessage, secretKey);
-
+var encryptedMessage = crypto.AES.encrypt(JSON.stringify(secretMessage), secretKey);
 console.log('This is the encrypted message: ' + encryptedMessage);
 
 // Decrypt message
 var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
-
 var decryptedMessage = bytes.toString(crypto.enc.Utf8);
 console.log('This is the decrypted message: ' + decryptedMessage);
+
+
+
